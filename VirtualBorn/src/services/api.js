27eaa9e7e9be20a31416d3a8d47baf39962a1b1d
Isabela Crivelli <isabela.crivelli@novaandradina.or>
2019-05-15 19:20:32 -0400

    Deixando só o servidor

--- VirtualBorn/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //baseURL: 'http://192.168.2.196:3001/api'
-    //baseURL: 'http://172.23.149.134:3001/api'
-    baseURL: 'http://172.23.149.134:3001/api'
-})
-
-export default api;
